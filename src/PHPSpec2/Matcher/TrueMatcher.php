bba2c1894410243006674873708bb67afb00cb2f
Marcello Duarte <marcello.duarte@gmail.com>
2012-08-19 18:15:12 +0100

    Removed usedAdlias unused property

--- src/PHPSpec2/Matcher/TrueMatcher.php
+++ src/PHPSpec2/Matcher/TrueMatcher.php
@@ -7,11 +7,9 @@ use PHPSpec2\Exception\Example\BooleanNotEqualException;
 class TrueMatcher extends BasicMatcher
 {
     private $actual;
-    private $usedAlias;
 
     public function supports($name, $subject, array $arguments)
     {
-        $this->usedAlias = $name;
         return in_array($name, array('be_true', 'return_true'));
     }
 
@@ -25,14 +23,14 @@ class TrueMatcher extends BasicMatcher
     {
         $type = $this->actual === false ? 'false' : gettype($this->actual);
         return new BooleanNotEqualException(
-            'Expected true got ' . $type . 'using (' . $this->usedAlias . ')', true, $this->actual
+            'Expected true got ' . $type . 'using (' . $name . ')', true, $this->actual
         );
     }
 
     protected function getNegativeFailureException($name, $subject, array $arguments)
     {
         return new BooleanNotEqualException(
-            'Expected not to be true, got true using (' . $this->usedAlias . ')', true, $this->actual
+            'Expected not to be true, got true using (' . $name . ')', true, $this->actual
         );
     }
 }
